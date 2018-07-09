c82a0ca0b44ee74808510f053e118462b91b116f
marcusp619 <marcusp619@gmail.com>
2018-07-09 15:10:47 -0600

    Add addArticle function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('test');
+function addArticle() {
+  var newArticle = document.createElement('article');
+  newArticle.classList.add('article');
+  newArticle.innerHTML = `<h4>${websiteTitle.value}</h4><hr><p>${websiteURL.value}</p><hr><div><button>Read</button><button>Delete</button></div>`;  
+  asideElement.appendChild(newArticle);
+}
+
+var asideElement = document.querySelector('aside');
+var websiteTitle = document.querySelector('#website-title-js');
+var websiteURL = document.querySelector('#website-url-js');
+var enterBtn = document.querySelector('#submit-btn-js');
+
+enterBtn.addEventListener('click', function() {
+  event.preventDefault();
+  addArticle();
+})
