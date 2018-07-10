ed917592b7362f461825dbd52ca14821d4d7d6f6
Ahmad Kayyali <kayyali18@gmail.com>
2018-07-09 18:49:12 -0600

    Add useless functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function addArticle() {
   var newArticle = document.createElement('article');
   newArticle.classList.add('article');
-  newArticle.innerHTML = `<h4>${websiteTitle.value}</h4><hr><p>${websiteURL.value}</p><hr><div><button class="read-btn">Read</button><button class="delete-btn">Delete</button></div>`;  
+  newArticle.innerHTML = `<h4>${websiteTitle.value}</h4><hr><p>${websiteURL.value}</p><hr><div><button class="read-btn">Read</button><button class="delete-btn">Delete</button></div>`;
   asideElement.appendChild(newArticle);
 }
 
@@ -10,23 +10,35 @@ var websiteTitle = document.querySelector('#website-title-js');
 var websiteURL = document.querySelector('#website-url-js');
 var enterBtn = document.querySelector('#submit-btn-js');
 var deleteBtn = document.querySelector('.delete-btn');
+var readArr = [];
 
 
 enterBtn.addEventListener('click', function() {
   event.preventDefault();
   addArticle();
   readBtn = document.querySelectorAll('.read-btn');
-  console.log(readBtn);
-  readBtns();  
+  // console.log(readBtn);
+  for (var btn of readBtn.entries()) {
+    readBtn.addEventListener('click', function () {
+      event.preventDefault();
+      console.log(this);
+      this.classList.toggle('read');
+    })
+  }
+  // readBtns();
 });
+
+
+
+
 
 function readBtns() {
   // debugger;
   for (var i = 0; i < readBtn.length; i++) {
     readBtn[i].addEventListener('click', function() {
       event.preventDefault();
-      readBtn[i].classList.toggle('read');
-      console.log(readBtn); 
-    });  
+      this.classList.toggle('read');
+      console.log(readBtn);
+    });
   }
 }
