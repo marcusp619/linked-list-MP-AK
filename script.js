098f28978474f03d3245d0d1be43bd5dd72edd81
marcusp619 <marcusp619@gmail.com>
2018-07-09 21:46:40 -0600

    Add delete darticle function

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 function addArticle() {
   var newArticle = document.createElement('article');
   newArticle.classList.add('article');
-  newArticle.innerHTML = `<h4>${websiteTitle.value}</h4><hr><p>${websiteURL.value}</p><hr><div><button class="read-btn">Read</button><button class="delete-btn">Delete</button></div>`;  
+  newArticle.innerHTML = `<h4>${websiteTitle.value}</h4><hr><p>${websiteURL.value}</p><hr><div><button class="read-btn">Read</button><button class="delete-btn" onclick="removeElement()">Delete</button></div>`;  
   asideElement.appendChild(newArticle);
+  var deleteBtn = document.querySelector('.delete-btn');
 }
 
+function removeElement() {
+  event.target.parentNode.parentNode.remove("article");
+}
+
+var enterBtn = document.querySelector('#submit-btn-js');
 var asideElement = document.querySelector('aside');
 var websiteTitle = document.querySelector('#website-title-js');
 var websiteURL = document.querySelector('#website-url-js');
-var enterBtn = document.querySelector('#submit-btn-js');
-var deleteBtn = document.querySelector('.delete-btn');
-
+// var readBtn = document.querySelector('.read-btn');
 
 enterBtn.addEventListener('click', function() {
   event.preventDefault();
-  addArticle();
-  readBtn = document.querySelectorAll('.read-btn');
-  console.log(readBtn);
-  readBtns();  
+  addArticle()
 });
 
-function readBtns() {
-  // debugger;
-  for (var i = 0; i < readBtn.length; i++) {
-    readBtn[i].addEventListener('click', function() {
-      event.preventDefault();
-      readBtn[i].classList.toggle('read');
-      console.log(readBtn); 
-    });  
-  }
-}
+
